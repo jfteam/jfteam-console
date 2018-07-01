27ecc1e92dcbd395923740323aeb5b01d9176846
fengwenping <244160190@163.com>
2018-07-01 17:59:19 +0800

    修改

--- jfteam-console-web/src/main/java/org/jfteam/console/web/controller/UserController.java
+++ jfteam-console-web/src/main/java/org/jfteam/console/web/controller/UserController.java
@@ -1,5 +1,12 @@
 package org.jfteam.console.web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -7,5 +14,12 @@ package org.jfteam.console.web.controller;
  * Date: 2017-09-11
  * Time: 下午9:36
  */
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+    @GetMapping
+    public List<String> list() {
+        return new ArrayList();
+    }
 }
