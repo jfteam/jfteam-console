040d5e28054ca7db328c5333fa3b255f961dd85d
fengwenping <244160190@163.com>
2018-07-01 17:58:03 +0800

    增加用户列表的rest服务

---
+++ jfteam-console-web/src/main/java/org/jfteam/console/web/controller/UserController.java
@@ -0,0 +1,11 @@
+package org.jfteam.console.web.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: fengwenping
+ * Date: 2017-09-11
+ * Time: 下午9:36
+ */
+public class UserController {
+}
